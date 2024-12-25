b0969feb785b8f6c5f1cb0723242e2d2f699a78b
helloasir <helloasir@gmail.com>
2024-12-25 22:44:55 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://helloasir.github.io',
+  base: 'astrojson',
   integrations: [tailwind()],
 });
